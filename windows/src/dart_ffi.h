07a002e547d3ae9abdca30567f09cf04c122efd4
dario windows desktop <daapplab@gmail.com>
2023-01-25 20:10:55 +0100

    added: windows mecab src
    This is necessary because mecab is not easily buildable on windows 64bit

---
+++ windows/src/dart_ffi.h
@@ -0,0 +1,18 @@
+
+#ifndef MECAB_DART_FFI_H_
+#define MECAB_DART_FFI_H_
+
+#include <vector>
+#include <string>
+
+std::vector<char*> split(std::string st);
+
+extern "C"  //__attribute__((visibility("default"))) __attribute__((used))
+{    
+    void* initMecab(const char* opt, const char* dicdir);
+    const char* parse(void* mecab, const char* input);
+    void destroyMecab(void* mecab);
+    int native_add(int x, int y);
+}
+
+#endif
